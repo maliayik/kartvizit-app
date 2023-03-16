23e17f4bf45a8b2590bf00d36ad907bc590d6fb1
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-03-16 00:06:19 +0000

    third commit

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
 
+  cardItem={
+    title:'Software Developer',
+    name:'Mehmet Ali AYIK',
+    phone:'0555 555 55 55',
+    email:'info@mehmetaliayik.com.tr',
+    adress:'Orhangazi,BURSA',
+
+  };
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
